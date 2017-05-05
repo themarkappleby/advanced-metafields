5f22e64c6dd2d44eaa9544622b1af82fde80e95e
themarkappleby <themarkappleby@gmail.com>
2017-05-04 21:34:54 -0400

    functional proof of concept

--- lambda/index.js
+++ lambda/index.js
@@ -5,9 +5,9 @@ const generateNonce = require('nonce')()
 
 // constants
 const app = express()
-const UNAUTHORIZED_CODE = 401
 const apiKey = process.env.apiKey
-const scopes = 'write_content,write_products'
+const sharedSecret = process.env.sharedSecret
+const UNAUTHORIZED_CODE = 401
 
 // enable cors
 app.use(function (req, res, next) {
@@ -16,40 +16,60 @@ app.use(function (req, res, next) {
   next()
 })
 
+app.get('/done', function (req, res) {
+  const options = {
+    uri: `https://${req.query.shop}/admin/oauth/access_token`,
+    method: 'POST',
+    json: {
+      client_id: apiKey,
+      client_secret: sharedSecret,
+      code: req.query.code
+    }
+  }
+  request(options, function (err, response, body) {
+    if (!err) {
+      const token = body['access_token']
+      res.redirect(`http://localhost:8080?token=${token}`)
+    }
+  })
+})
+
 // catch all incoming requests
 app.all('*', function (req, res) {
-  if (req.url === '/auth') {
-    auth(res)
-  } else {
+  if (req.url.includes('myshopify')) {
+    const token = req.query.token
     res.setHeader('Content-Type', 'application/json')
     request({
       uri: prepURL(req.url),
       method: 'GET',
       headers: {
-        'X-Shopify-Access-Token': 'f63deb030a800c88c6efb01a2fdc7147'
+        'X-Shopify-Access-Token': token
       }
     }, function (err, response, body) {
       if (response.statusCode === UNAUTHORIZED_CODE) {
-        body = JSON.parse(body)
-        body.statusCode = response.statusCode
-        body.statusMessage = response.statusMessage
-        body.redirect = getAuthURL()
-        res.send(body)
+        redirectForAuth(body, res, response)
       } else if (!err) {
         res.send(body)
       }
     })
+  } else {
+    res.send('app')
   }
 })
 
-function auth (res) {
-  const nonce = generateNonce().toString()
-  res.redirect(`https://developmentsandbox.myshopify.com/admin/oauth/authorize?client_id=${apiKey}&scope=${scopes}&redirect_uri=http://localhost:3000/done&state=${nonce}`)
+function redirectForAuth (body, res, response) {
+  body = JSON.parse(body)
+  body.statusCode = response.statusCode
+  body.statusMessage = response.statusMessage
+  body.redirect = getAuthURL()
+  res.send(body)
 }
 
 // get Shopify authorization URL
 function getAuthURL () {
+  const scopes = 'write_content,write_products'
   const nonce = generateNonce().toString()
+
   return `https://developmentsandbox.myshopify.com/admin/oauth/authorize?client_id=${apiKey}&scope=${scopes}&redirect_uri=http://localhost:3000/done&state=${nonce}`
 }
 
